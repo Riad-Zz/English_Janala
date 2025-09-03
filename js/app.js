f044d7e56a784adf4d7cc7f49c75f0634c8f3bc5
Riad <kazuha3242@gmail.com>
2025-09-04 01:55:00 +0600

    feat:Lesson wise word container(word->fetch from api) implemented

--- js/app.js
+++ js/app.js
@@ -26,8 +26,32 @@ const lessonButtonGenerator = (allData) =>{
 const levelWiseWord = (level) =>{
     const url = `https://openapi.programming-hero.com/api/level/${level}`
     fetch(url).then(res => res.json())
-              .then(data => console.log(data.data)) ;
+              .then(data => wordCardGenerator(data.data)) ;
 }
 
-//-----------------Lesson wise Word container------------------------
- 
+//-----------------Lesson wise Word appeding------------------------
+const wordCardGenerator = allWord =>{
+    const wordContainerParent = document.getElementById('word-container') ;
+    wordContainerParent.innerHTML = "" ;
+    allWord.forEach(word => {
+        console.log(word) ;
+        const newWordCard = document.createElement('div') ;
+        newWordCard.innerHTML = `
+           <div class="word-card bg-white rounded-xl text-center">
+            <p class="font-bold text-3xl pt-5">${word.word}</p>
+            <p class="text-xl font-medium py-6">Meaning / Pronounciation</p>
+            <p class="bangla-font font-semibold text-xl">"${word.meaning} / ${word.pronunciation}"</p>
+            <div class="flex justify-around py-5">
+              <div>
+                <button class="btn btn-square bg-[#1a91ff1a]"> <i class="fa-solid fa-info"></i>
+</button>
+              </div>
+              <div>
+                <button class="btn btn-square bg-[#1a91ff1a]"><i class="fa-solid fa-volume-high"></i></button>
+              </div>
+            </div>
+          </div>
+        `
+        wordContainerParent.appendChild(newWordCard) ;
+    })
+}
