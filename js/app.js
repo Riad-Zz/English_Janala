6d6e53e241cbe0c4361284057ca904c87376a2b7
Riad <kazuha3242@gmail.com>
2025-09-04 16:20:29 +0600

    feat:Currently active button indicator added

--- js/app.js
+++ js/app.js
@@ -15,30 +15,62 @@ const lessonButtonGenerator = (allData) =>{
         const newLesson = document.createElement('div') ;
         newLesson.innerHTML = `
           <div>
-          <button onclick="levelWiseWord(${data.level_no})" class="btn btn-outline text-[#422AD5]"><i class="fa-solid fa-book-open"></i>Lesson-${data.level_no}</button>
+          <button  id ="lesson-btn-${data.level_no}" onclick="levelWiseWord(${data.level_no})" class="btn btn-outline text-[#422AD5]"><i class="fa-solid fa-book-open"></i>Lesson-${data.level_no}</button>
         </div>
         `
+        newLesson.classList.add("lesson-btn")
         parentLessonContainer.appendChild(newLesson) ;
     })
 }
+
 
 //---------------Get word for specific lessson on-click------------------------- 
 const levelWiseWord = (level) =>{
     const url = `https://openapi.programming-hero.com/api/level/${level}`
     fetch(url).then(res => res.json())
-              .then(data => wordCardGenerator(data.data)) ;
+              .then(data => {
+                activeById(level) ;
+                wordCardGenerator(data.data) ;
+              }) ;
+}
+
+//--------------------Button Activate on Click-----------------------
+const activeById = id =>{
+  const allButtons = document.querySelectorAll(".lesson-btn div button") ;
+  //Remove all Active
+  console.log(allButtons) ;
+  for(const btn of allButtons){
+    btn.classList.remove('active') ;
+  }
+  // Clicked Active button  
+  const activeButton = document.getElementById(`lesson-btn-${id}`) ;
+  activeButton.classList.add('active') ;
+  
 }
 
 //-----------------Lesson wise Word appeding------------------------
 const wordCardGenerator = allWord =>{
     const wordContainerParent = document.getElementById('word-container') ;
     wordContainerParent.innerHTML = "" ;
+    // console.log(allWord.length) ;
+    if(allWord.length == 0){
+      const NoWordMessage = document.createElement('div') ;
+      NoWordMessage.classList.add('col-span-3','flex' , 'justify-center','flex-col','items-center') ;
+      // NoWordMessage.classList.add('flex') ;
+      NoWordMessage.innerHTML = `
+            <img src="assets/alert-error.png" alt="" class="mb-2">
+            <p class="bangla-font text-[#79716B] text-sm">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <p class="bangla-font text-[#292524] text-xl font-medium py-2">নেক্সট Lesson এ যান</p>
+      `
+      wordContainerParent.appendChild(NoWordMessage) ;
+      return ;
+    }
     allWord.forEach(word => {
-        console.log(word) ;
+        // console.log(word) ;
         const newWordCard = document.createElement('div') ;
         newWordCard.innerHTML = `
-           <div class="word-card bg-white rounded-xl text-center">
-            <p class="font-bold text-3xl pt-5">${word.word}</p>
+           <div class="word-card bg-white rounded-xl text-center px-3">
+            <p class="font-bold text-2xl pt-5">${word.word}</p>
             <p class="text-xl font-medium py-6">Meaning / Pronounciation</p>
             <p class="bangla-font font-semibold text-xl">"${word.meaning} / ${word.pronunciation}"</p>
             <div class="flex justify-around py-5">
