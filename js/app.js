4460f3986c95a3be06189017a16f630ff54dfc5b
Riad <kazuha3242@gmail.com>
2025-09-04 18:29:44 +0600

    feat : Word Details Modal Added

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ const levelWiseWord = (level) =>{
 const activeById = id =>{
   const allButtons = document.querySelectorAll(".lesson-btn div button") ;
   //Remove all Active
-  console.log(allButtons) ;
+  // console.log(allButtons) ;
   for(const btn of allButtons){
     btn.classList.remove('active') ;
   }
@@ -65,17 +65,18 @@ const wordCardGenerator = allWord =>{
       wordContainerParent.appendChild(NoWordMessage) ;
       return ;
     }
+
     allWord.forEach(word => {
         // console.log(word) ;
         const newWordCard = document.createElement('div') ;
         newWordCard.innerHTML = `
            <div class="word-card bg-white rounded-xl text-center px-3">
-            <p class="font-bold text-2xl pt-5">${word.word}</p>
+            <p class="font-bold text-2xl pt-5">${word.word ? word.word : "Not found"} </p>
             <p class="text-xl font-medium py-6">Meaning / Pronounciation</p>
-            <p class="bangla-font font-semibold text-xl">"${word.meaning} / ${word.pronunciation}"</p>
+            <p class="bangla-font font-semibold text-xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যাই নাই"} / ${word.pronunciation ? word.pronunciation : "পাওয়া যাই নাই"}"</p>
             <div class="flex justify-around py-5">
               <div>
-                <button class="btn btn-square bg-[#1a91ff1a]"> <i class="fa-solid fa-info"></i>
+                <button onclick="loadWord(${word.id})" class="btn btn-square bg-[#1a91ff1a]"> <i class="fa-solid fa-info"></i>
 </button>
               </div>
               <div>
@@ -86,4 +87,51 @@ const wordCardGenerator = allWord =>{
         `
         wordContainerParent.appendChild(newWordCard) ;
     })
+}
+
+// Load Word Details 
+const loadWord = (wordId) => {
+  const wordUrl = `https://openapi.programming-hero.com/api/word/${wordId}` ;
+  fetch(wordUrl).then(res => res.json()).then(data => displayWordDetails(data.data)) ;
+}
+
+//Word Details Display 
+const displayWordDetails = allDetails =>{
+  console.log(allDetails) ;
+  const modalBox = document.getElementById('my_modal_5') ;
+  const modalBoxAppend = document.getElementById('modal-box-parent') ;
+  modalBoxAppend.innerHTML='' ;
+  modalBoxAppend.innerHTML = `
+       <div class="area p-6 border border-blue-400 rounded-xl">
+      <p class="text-2xl pb-4 font-bold">${allDetails.word} (<i class="fa-solid fa-microphone"></i> : ${allDetails.pronunciation})</p>
+      <p class="font-semibold text-xl">Meaning</p>
+      <p class="bangla-font mt-2 mb-4 font-medium text-xl">${allDetails.meaning}</p>
+      <p class="font-semibold text-xl">Example</p>
+      <p class="text-[#000000] text-sm pt-2 mb-4">${allDetails.sentence}</p>
+      <p class="bangla-font font-medium mb-2">সমার্থক শব্দ গুলো</p>
+      <div class="flex gap-2 flex-wrap " id ="synonyms">
+      </div>
+    </div>
+    <div class="modal-action justify-start">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+         
+         <button class="btn btn-active bg-[#422AD5] text-white">Complete Learning</button>
+      </form>
+    </div>
+  `
+  allSynonymsAppend(allDetails.synonyms) ;
+  modalBox.showModal() ;
+}
+
+const allSynonymsAppend = allSynonyms =>{
+  const parent = document.getElementById('synonyms') ;
+  for(const syn of allSynonyms){
+    console.log(syn) ;
+    const synonymButton = document.createElement('button') ;
+    synonymButton.innerHTML = `
+          <button class="btn bg-[#EDF7FF]">${syn}</button>
+    `
+    parent.appendChild(synonymButton) ;
+  }
 }
